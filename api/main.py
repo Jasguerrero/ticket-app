54f3f9a7af3833e4fd962292d64132df8aef3ccc
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2025-03-31 19:53:09 -0600

    add prometheus

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from api import create_app
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = create_app()
 
+# Use a different path to avoid conflicts
+metrics = PrometheusMetrics(app)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
